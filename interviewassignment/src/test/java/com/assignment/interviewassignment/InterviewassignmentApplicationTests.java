5a67dc00ab09fbb14e570f3cf70ac3e4c6f1d319
TechGeek300593 <narendhranmay30@gmail.com>
2020-07-16 05:38:39 +0530

    assignment
    assignment code

---
+++ interviewassignment/src/test/java/com/assignment/interviewassignment/InterviewassignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.interviewassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewassignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
